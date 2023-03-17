e89c7814b97c734a7b40fd2441517337354c3e87
Naresh Oli <olinaresh85@gmail.com>
2023-03-16 23:49:19 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
 
-// Write your code here
+let random = Math.floor(Math.random()*6)+1
+
+let num3 = 24
+let num4 = 10
+let mod = num3%num4
+
+
+let max = Math.max(1,2,20)
